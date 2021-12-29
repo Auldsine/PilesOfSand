acfd96c83c1b1fc8b062e2cc8ebd6bdf4a6a4d0b
ta2e3 <mohamed_taie@hotmail.com>
2021-12-29 11:43:33 +0200

    3dlt grounds lvl 2 (ta23)

--- Assets/Scripts/RavenDialoguewall.cs
+++ Assets/Scripts/RavenDialoguewall.cs
@@ -21,7 +21,7 @@ public class RavenDialoguewall : MonoBehaviour
         if(other.tag=="player"){
             string[] dialogue={"Raven: The wall needs explosives to be destroyed, Do you have any?",
             "Yonoko: No but I have what is better.",
-            "Raven: There is an apple that can restore your strength behind the wall"};
+            "Raven: There is an apple that can restore your strength above that tree behind the wall"};
             dialogueManager.SetSentences(dialogue);
             dialogueManager.StartCoroutine(dialogueManager.TypeDialogue());
         }
